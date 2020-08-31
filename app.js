20587035ae710181cbce69be3c0fc6a82814e7d7
Idah <idah.koome@strathmore.edu>
2020-08-31 22:45:10 +0300

    Setup tooling - ESLint, Nodemon

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const express = require('express'); //import express
+/* eslint-disable linebreak-style */
+const express = require('express'); // import express
 
-const app = express(); //initialize express
+const app = express(); // initialize express
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.render('hello.ejs');
+});
+
+app.get('/top', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Running on port ${port}`);
+});
