e0c5c94626a44093ba8771adf0b813fafe870d2e
Idah <idah.koome@strathmore.edu>
2020-09-02 01:01:09 +0300

    code cleanup

--- app.js
+++ app.js
@@ -5,49 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express(); // initialize express
 const db = mongoose.connect('mongodb://localhost/bookAPI'); // connect to a database on mongo
-const bookRouter = express.Router(); // create books route
+ 
 const port = process.env.PORT || 3000;
 
 const Book = require('./models/bookModel.js'); // Book Model
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());;
-
-// access all books route via : /api/books
-// querying database to locate data
-// also allows searching/filtering of the data based n genre search string
-bookRouter.route('/books') 
-  .post((req, res) => {
-    const book =  new Book(req.body);
-
-    book.save();
-    return res.status(201).json(book); //send status and the created book
-  })
-  .get((req, res) => {
-    const query = {};
-    if(req.query.genre){
-      query.genre = req.query.genre;
-    }
-    Book.find((err, books) => {
-    if(err){
-      return res.send(err);
-    } 
-      return res.json(books);                            
-    })
-  });
-
-// route to get a single book
-bookRouter.route('/books/:bookId') 
-  .get((req, res) => {
-    
-    Book.findById(req.params.bookId, (err, book) => {
-    if(err){
-      return res.send(err);
-    } 
-      return res.json(book);                            
-    })
-  });
-
 //access the BookRouter via /api in the URL
 app.use('/api', bookRouter);
 
