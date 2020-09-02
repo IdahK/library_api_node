2d76fe0f3c5aa041f46931d04edea4442ac9be95
Idah <idah.koome@strathmore.edu>
2020-09-02 12:54:13 +0300

    Patch functionality for Book

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -5,7 +5,6 @@ function routes(Book){
 
 const bookRouter = express.Router();
 
-
 bookRouter.route('/books') 
   // create a new book  
   .post((req, res) => {
@@ -29,32 +28,62 @@ bookRouter.route('/books')
     })
   });
 
-// route to get a single book based on Book Id
-bookRouter.route('/books/:bookId') 
-    .get((req, res) => {
-    
-        Book.findById(req.params.bookId, (err, book) => {
+//create a middleware to handle routes with book Id
+//it does the search for the book of that Id separately
+bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
         if(err){
             return res.send(err);
-        } 
-            return res.json(book);                            
-        });
+        }
+        if(book){
+            req.book = book;
+            return next();
+        }
+        return res.sendStatus(404); //if book is not found, send this status
     })
+})
+
+
+// route for a single book based on Book Id
+bookRouter.route('/books/:bookId') 
+    //get a single book based on Id
+    .get((req, res) => res.json(req.book))
+
     //implement put/update for a book
     .put((req, res) => {
-        Book.findById(req.params.bookId, (err, book) => {
+        const { book } = req;;
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.genre = req.body.genre;
+        book.read = req.body.read;
+        req.book.save((err)=>{
             if(err){
                 return res.send(err);
-            } 
-                book.title = req.body.title;
-                book.author = req.body.author;
-                book.genre = req.body.genre;
-                book.read = req.body.read;
-                book.save();
-                return res.json(book); 
-
+            }
+            return res.json(book);
         });
-    })
+        })
+    
+    //implement patch (update specific items) for the book
+    .patch((req, res) => {
+        const { book } = req;
+        // eslint-disable--next-line no-underscore-dangle
+        if(req.body._id){ //remove if id is sent
+            // eslint-disable--next-line no-underscore-dangle
+            delete req.body._id;
+        }
+        Object.entries(req.body).forEach((item) => {
+            const key = item[0];;
+            const value = item[1];
+            book[key] = value;
+        });
+        req.book.save((err)=>{
+            if(err){
+                return res.send(err);
+            }
+            return res.json(book);
+        });
+    });
 
 return bookRouter;
 }
