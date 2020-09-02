9b35ff98acfbf88f51f71fdec4e956b4acd19068
Idah <idah.koome@strathmore.edu>
2020-09-02 12:23:05 +0300

    PUT(Update) Book Functionality

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,17 +1,21 @@
+/** eslint-disable no-param-reassign */
 const express = require('express');
 
 function routes(Book){
 
 const bookRouter = express.Router();
 
-// access all books route via : /api/books, search by genre
+
 bookRouter.route('/books') 
+  // create a new book  
   .post((req, res) => {
     const book =  new Book(req.body);
 
     book.save();
     return res.status(201).json(book); //send status and the created book
   })
+
+  // access all books route via : /api/books, search by genre
   .get((req, res) => {
     const query = {};
     if(req.query.genre){
@@ -25,17 +29,32 @@ bookRouter.route('/books')
     })
   });
 
-// route to get a single book
+// route to get a single book based on Book Id
 bookRouter.route('/books/:bookId') 
-  .get((req, res) => {
+    .get((req, res) => {
     
-    Book.findById(req.params.bookId, (err, book) => {
-    if(err){
-      return res.send(err);
-    } 
-      return res.json(book);                            
+        Book.findById(req.params.bookId, (err, book) => {
+        if(err){
+            return res.send(err);
+        } 
+            return res.json(book);                            
+        });
     })
-  });
+    //implement put/update for a book
+    .put((req, res) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if(err){
+                return res.send(err);
+            } 
+                book.title = req.body.title;
+                book.author = req.body.author;
+                book.genre = req.body.genre;
+                book.read = req.body.read;
+                book.save();
+                return res.json(book); 
+
+        });
+    })
 
 return bookRouter;
 }
